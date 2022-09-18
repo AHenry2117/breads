680aaff4f536d9279913391b31b5e25ab211e5aa
AHenry2117 <Aehenry2117@gmail.com>
2022-09-18 12:41:46 -0700

    sending index and show data

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
 // DEPENDENCIES
-const express = require('express')
+const express = require('express');
 
 // CONFIGURATION
-require('dotenv').config()
-const PORT = process.env.PORT
-const app = express()
+require('dotenv').config();
+const PORT = process.env.PORT;
+const app = express();
 // MIDDLEWARE
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+app.use(express.static('public'));
 
 // ROUTES
 app.get('/', (req, res) => {
-  res.send('Welcome to an Awesome App about Breads!')
+  res.send('Welcome to an Awesome App about Breads!');
+});
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
 })
-  // Breads
-  const breadsController = require('./controllers/breads_controller.js')
-  app.use('/breads', breadsController)
+// Breads
+  const breadsController = require('./controllers/breads_controller.js');
+  app.use('/breads', breadsController);
 // LISTEN
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
-})
+});
  
   
